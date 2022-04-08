8554f2c53f5520a54b00763e80b8810b59f20338
Tracy Michaels <tracylmichaels@gmail.com>
2022-04-08 01:19:58 -0400

    formatting and linting

--- models.py
+++ models.py
@@ -4,6 +4,7 @@ from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 from app import db
 
+
 class User(db.Model, UserMixin):
     """This is the database table for the User"""
 
