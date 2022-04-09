10f5c3356e6eb8c03adbd4c1db13a60fcd6c6a2b
Preetham <sthelluri1@student.gsu.edu>
2022-04-08 22:47:43 -0400

    Added the last database change

--- app.py
+++ app.py
@@ -8,6 +8,8 @@ load_dotenv(find_dotenv())
 
 app = flask.Flask(__name__)
 database = os.getenv("DATABASE_URL")
+if database.startswith("postgres://"):
+    database = database.replace("postgres://", "postgresql://", 1)
 app.config["SQLALCHEMY_DATABASE_URI"] = database
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.secret_key = b"Nothing in here, this is to protect from de-frauding"
